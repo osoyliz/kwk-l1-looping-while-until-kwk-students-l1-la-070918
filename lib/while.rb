e1086b29aa539e7158728efbd37d7f6660dc7ca2
osoyliz <lizosoy18@gmail.com>
2018-07-10 22:50:57 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -3,9 +3,9 @@ def using_while
 	while levitation_force<10
 	puts"Wingardium Leviosa"
 	levitation_force+=1
-	puts "your levitation force is now at #{levitation}."
+	puts "your levitation force is now at #{levitation_force}."
 	end
-	puts "you are now leavition"
+	puts "you are now leavitating a feather"
 end
 using_while
 
